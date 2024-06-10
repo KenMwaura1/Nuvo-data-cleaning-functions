0e6ec6d5058824a964f641d066fe2fe2d9651328
Kennedy Ngugi Mwaura <kemwaura@gmail.com>
2024-06-10 19:51:57 +0300

    Update CleanOutput.tsx

--- src/components/CleanOutput.tsx
+++ src/components/CleanOutput.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import { FC, useState } from "react";
 import { IHandlerFC } from "../types/HandlerFC";
 import { ColumnAPI, NuvoImporter, EntryChangeResult } from "nuvo-react";
 
